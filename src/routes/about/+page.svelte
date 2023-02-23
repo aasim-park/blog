18e7ad7c25c2f1a8775121e990a8ad82f82b15c1
Aasim Shaikh <62887869+aasim-park@users.noreply.github.com>
2023-02-23 11:01:15 +0530

    About (#1)
    * font roboto
    
    * about intial changes
    
    * completed about page
    
    ---------
    
    Co-authored-by: Aasim <aasimshaikh1993@gmail.com>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,51 @@
+<script>
+	import resume from '$lib/resume/Rezume.pdf';
+</script>
+
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About this Website" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About</h1>
+	<div class="hello">Hello !</div>
+	<div class="name">I am Aasim Shaikh.</div>
+	<div>
+		I am a Software Developer living in Mumbai, India. Pursuing to be a cloud engineer. I have
+		experience in
+		<ul>
+			<li>Setup cloud VMs in <b>GCP, AWS, and digital ocean</b> cloud environments.</li>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+			<li>Make a <b>Dockerfile</b> and orchestrate it using <b>docker-compose</b>.</li>
+			<li>Hands-on exprience on <b>Kubernetes cluster</b> in GCP and AWS.</li>
+			<li><b>S3</b> for hosting static pages.</li>
+			<li><b>Cloud Run</b> for running container-based workload.</li>
+			<li><b>ECS</b> for Kubernetes-like experience on AWS.</li>
+			<li>
+				Database management in the cloud handled workload for DocumentDB (MongoDB on AWS), MYSQL
+				instances in AWS and DigitalOcean.
+			</li>
+			<li>
+				Setup pipeline in GCP to setup CI/CD using <b>code build</b> and Cloud Run.
+			</li>
+		</ul>
+	</div>
+	<div class="name">Contact.</div>
+	<div>
+		<a href={resume}> Resume </a>
+		<a href="mailto: asimshaikh1993@gmail.com?subject = Contact"> Send email </a>
+	</div>
 </div>
+
+<style>
+	h1 {
+		font-size: 60px;
+	}
+	.hello {
+		font-size: 40px;
+	}
+	.name {
+		font-size: 30px;
+	}
+</style>
