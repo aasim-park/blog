75d14d11351b44e17213817a1ee912ff51ef0cd8
Aasim <aasimshaikh1993@gmail.com>
2023-02-27 11:40:04 +0530

    typography added

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	export let data;
 	const post = data.slug;
-	console.log(post);
 </script>
 
 <h1>{post[0].title}</h1>
 <h2>{post[0].excerpt}</h2>
-<h3>{post[0].description}</h3>
+<article class="prose lg:prose-xl">
+	{post[0].description}
+</article>
