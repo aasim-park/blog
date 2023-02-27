cebca1ec8695e408e22f8f591a9cb7f3a5193f49
Aasim <aasimshaikh1993@gmail.com>
2023-02-27 16:03:27 +0530

    about page formatting

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,18 +10,24 @@
 <div class="text-column">
 	<div class="title">About</div>
 	<div class="text-justify p-3">
-		Hello! My name is <span class="text-2xl underline underline-offset-4 decoration-[#ff3e00] "
-			>Aasim Shaikh
-		</span>and I am a software developer based in Mumbai. My area of expertise is cloud engineering,
-		and I have experience setting up cloud virtual machines (VMs) in GCP, AWS, and Digital Ocean
-		environments. I am also skilled in creating Docker files and orchestrating them using Docker
-		Compose. For container workloads, I have experience using AWS services such as EKS and ECS, and
-		in GCP, I have deployed container workloads on GKE standard, Cloud Run, and App Engine. In terms
-		of CI/CD, I have used AWS Pipeline and Cloud Build in GCP. For database management, I have set
-		up cloud-specific databases such as Amazon RDS and DocumentDB. For basic setups, I create VMs
-		for websites, set up networking with private and public routing options, and create shell
-		scripts to unify workloads. With respect to source code management (SCM), I have experience
-		using Git, GitHub, Bitbucket, and GitLab, as well as cloud-specific SCMs. Other cloud tools I
+		Hello! My name is <span class="underline-about">Aasim Shaikh </span>and I am a software
+		developer based in Mumbai. My area of expertise is cloud engineering, and I have experience
+		setting up cloud virtual machines (VMs) in <span class="underline-about"> GCP, AWS,</span> and
+		<span class="underline-about">Digital Ocean</span>
+		environments. I am also skilled in creating Docker files and orchestrating them using Docker Compose.
+		For container workloads, I have experience using AWS services such as
+		<span class="underline-about">EKS </span>and <span class="underline-about">ECS</span> , and in
+		GCP, I have deployed container workloads on GKE standard,
+		<span class="underline-about"> Cloud Run </span>, and
+		<span class="underline-about"> App Engine </span>
+		. In terms of CI/CD, I have used AWS Pipeline and
+		<span class="underline-about">Cloud Build</span>
+		in GCP. For database management, I have set up cloud-specific databases such as Amazon RDS and DocumentDB.
+		For basic setups, I create VMs for websites, set up networking with private and public routing options,
+		and create shell scripts to unify workloads. With respect to source code management (SCM), I have
+		experience using
+		<span class="underline-about">Git, GitHub, Bitbucket </span>, and
+		<span class="underline-about">GitLab</span>, as well as cloud-specific SCMs. Other cloud tools I
 		have used include S3, Secret Manager, Elastic Beanstalk, Cloud Functions, Lambda, and various
 		microservices and serverless tools.
 	</div>
--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,10 +1,10 @@
 import { post } from '$db/post';
 import { compile } from 'mdsvex';
-import { ObjectId } from "mongodb"
+import { ObjectId } from 'mongodb';
 
 export const load = async function ({ params }) {
-	const id = new ObjectId(params.slug)
-	const response = await post.find({"_id":id}).toArray();
+	const id = new ObjectId(params.slug);
+	const response = await post.find({ _id: id }).toArray();
 	const compileResponse = await compile(response[0].description);
 	return {
 		slug: compileResponse
