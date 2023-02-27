034db0b1e0b36825429f3d5b392fa672c6ec1a33
Aasim Shaikh <62887869+aasim-park@users.noreply.github.com>
2023-02-27 17:02:56 +0530

    Bug fixes (#8)
    * Create docker-image.yml
    
    * bugfixes
    
    * docker args check file
    
    * updated cloud build to secret amanger
    
    * new step in cloud-build
    
    * trial commit
    
    * updated yaml cloudbuild
    
    * check build
    
    * added env in cloudbuild
    
    * make env on fly
    
    * docker entrypoint bash and format and lint
    
    * doublequote and singlequote mistake
    
    * correct formatting
    
    * formatting
    
    * last try
    
    * print env
    
    * final bug fixed and deployed on new service
    
    * TODO for _id
    
    * lint and formated
    
    * trying with ARG in cloudbuild
    
    * corrected format
    
    * without build-arg
    
    * removed consolelog and added about from AI
    
    * tailwind added
    
    * style lang to postcss
    
    * about page fixes style and trying tailwindcss
    
    * typography added
    
    * working blogs format and lint
    
    * about formating and clean up
    
    * _id fixed
    
    * about page formatting
    
    * fixed mdsvex as main dependencies
    
    ---------
    
    Co-authored-by: Aasim <aasimshaikh1993@gmail.com>

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -3,7 +3,6 @@ import { post } from '$db/post';
 export const load = async function () {
 	const data = await post.find({}).toArray();
 	const parseData = await JSON.parse(JSON.stringify(data));
-	// console.log(data);
 	return {
 		posts: parseData
 	};
